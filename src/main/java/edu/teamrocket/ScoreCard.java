fc66e31519804dbff1391e9a3d72fa44e57a7941
Juan071825 <juanmateoalvarez@gmail.com>
2026-04-15 14:26:10 +0200

    feat(ScoreCard.java): :sparkles: Añadir nuevos métodos y continuar con frameListCreator.

--- src/main/java/edu/teamrocket/ScoreCard.java
+++ src/main/java/edu/teamrocket/ScoreCard.java
@@ -26,20 +26,66 @@ public class ScoreCard {
         }
     }
 
-    public String[] frameCreator(){
 
-        String[] frame_list = new String[numThrows()];
 
-        for(Byte indexThrow = 0; indexThrow < numThrows();){
-            if (extractThrow(indexThrow) != Simbols.FAUL.getSimbol() 
-            || extractThrow(indexThrow) != Simbols.SPARE.getSimbol()
-            || extractThrow(indexThrow) != Simbols.STRIKE.getSimbol()){
-                Character[] frame = new Character[2];
-                frame[indexThrow] = extractThrow(indexThrow);
-                frame[indexThrow + 1] = extractThrow(indexThrow);
-                indexThrow = indexThrow + (Byte)2;
+    /*public Character[] selectFrame (Character[][] match, Byte index){
+        Character[] frame = match[index];
+        return frame;
+    }*/
+
+
+        public Character[] frameCreator(Byte initialThrow, Byte numThrows){
+            Character[] frame = new Character[numThrows];
+            for ( Byte indexThrow = initialThrow; indexThrow < (initialThrow + 3); indexThrow++ ){
+                frame[initialThrow]
             }
         }
+
+    public Character[][] frameListCreator(){
+
+        Character[][] match = new Character[10][];
+        Byte indexThrow;
+
+        for(Byte indexMatch = 0; indexMatch < 10; indexMatch++){
+            Byte twoThrows = 2;
+            Byte oneThrow = 1;
+
+            // Calcula frames del 1 al 9.
+            if (indexMatch < 10){
+                if (extractThrow(indexThrow) != Simbols.STRIKE.getSimbol()){
+                    match[indexMatch] = new Character[2];
+                    match[indexMatch][0] = extractThrow(indexThrow);
+                    match[indexMatch][1] = extractThrow(indexThrow);
+                    indexThrow = (byte)(indexThrow + oneThrow);
+                } else {
+                    match[indexMatch] = new Character[1];
+                    match[indexMatch][0] = extractThrow(indexThrow);
+                    indexThrow = (byte)(indexThrow + twoThrows);
+                }
+
+
+            } else {
+                if(extractThrow(indexThrow) != Simbols.STRIKE.getSimbol()){
+                    match[indexMatch] = new Character[2];
+                    match[indexMatch][0] = extractThrow(indexThrow);
+                    match[indexMatch][1] = extractThrow(indexThrow);
+                } else if (extractThrow((byte)(indexThrow + oneThrow)) == Simbols.SPARE.getSimbol()) {
+                    match[indexMatch] = new Character[3];
+                    match[indexMatch][0] = extractThrow(indexThrow);
+                    match[indexMatch][1] = extractThrow(indexThrow);
+                    match[indexMatch][2] = extractThrow(indexThrow);                    
+                } else {
+                    match[indexMatch] = new Character[3];
+                    match[indexMatch][0] = extractThrow(indexThrow);
+                    match[indexMatch][1] = extractThrow(indexThrow);
+                    match[indexMatch][2] = extractThrow(indexThrow);
+                }
+            }
+        }
+
+        return frame_list;
+
+
     }
 
 
